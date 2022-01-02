569dd08782e2f2fd94bc64ac5c655a7de0d075bc
Biki <biki.hamzah@gmail.com>
2022-01-02 15:31:51 +0700

    ADD fitur dashboard and preview dataset

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,39 +8,9 @@
       :mini-variant.sync="mini"
       permanent
       app
+      color="#013A63"
     >
-      <v-list-item class="px-2">
-        <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-title>John Leider</v-list-item-title>
-
-        <v-btn
-          icon
-          @click.stop="mini = !mini"
-        >
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item
-          v-for="item in menus"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+      <Sidebar :mini.sync="mini"/>
     </v-navigation-drawer>
     <v-main>
       <!-- Provides the application the proper gutter -->
@@ -58,18 +28,17 @@
 </template>
 
 <script>
+import Sidebar from './components/sidebar/sidebar.vue';
 
 export default {
   name: 'App',
+  components: {
+    Sidebar,
+  },
 
   data: () => ({
     drawer: false,
     mini: true,
-    menus: [
-      { title: 'Home', icon: 'mdi-home-city' },
-      { title: 'My Account', icon: 'mdi-account' },
-      { title: 'Users', icon: 'mdi-account-group-outline' },
-    ],
   }),
 };
 </script>
---
+++ client/src/views/Dataset.vue
@@ -0,0 +1,168 @@
+<template>
+  <v-card>
+    <div class="py-4">
+      <v-card-title class="text-h4 font-weight-bold">
+        Dataset Bitcoin ( BTC - USD )
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="dataset"
+        :items-per-page="5"
+      ></v-data-table>
+    </div>
+  </v-card>
+</template>
+<script>
+
+export default {
+  name: 'Dataset',
+  data: () => ({
+    headers: [
+      {
+        text: 'Timestamp',
+        value: 'timestamp',
+      },
+      {
+        text: 'Open',
+        value: 'open',
+      },
+      {
+        text: 'Close',
+        value: 'close',
+      },
+      {
+        text: 'High',
+        value: 'high',
+      },
+      {
+        text: 'Low',
+        value: 'low',
+      },
+      {
+        text: 'Volume (BTC)',
+        value: 'volumeBtc',
+      },
+      {
+        text: 'Volume (Currency)',
+        value: 'volumeCurrency',
+      },
+      {
+        text: 'Weighted Price',
+        value: 'weightedPrice',
+      },
+    ],
+    dataset: [
+      {
+        timestamp: 100000,
+        open: NaN,
+        close: NaN,
+        high: 1000,
+        low: 99,
+        volumeBtc: 1000000,
+        volumeCurrency: 30000,
+        weightedPrice: 2,
+      },
+      {
+        timestamp: 100000,
+        open: NaN,
+        close: NaN,
+        high: 1000,
+        low: 99,
+        volumeBtc: 1000000,
+        volumeCurrency: 30000,
+        weightedPrice: 2,
+      },
+      {
+        timestamp: 100000,
+        open: NaN,
+        close: NaN,
+        high: 1000,
+        low: 99,
+        volumeBtc: 1000000,
+        volumeCurrency: 30000,
+        weightedPrice: 2,
+      },
+      {
+        timestamp: 100000,
+        open: NaN,
+        close: NaN,
+        high: 1000,
+        low: 99,
+        volumeBtc: 1000000,
+        volumeCurrency: 30000,
+        weightedPrice: 2,
+      },
+      {
+        timestamp: 100000,
+        open: NaN,
+        close: NaN,
+        high: 1000,
+        low: 99,
+        volumeBtc: 1000000,
+        volumeCurrency: 30000,
+        weightedPrice: 2,
+      },
+      {
+        timestamp: 100000,
+        open: NaN,
+        close: NaN,
+        high: 1000,
+        low: 99,
+        volumeBtc: 1000000,
+        volumeCurrency: 30000,
+        weightedPrice: 2,
+      },
+      {
+        timestamp: 100000,
+        open: NaN,
+        close: NaN,
+        high: 1000,
+        low: 99,
+        volumeBtc: 1000000,
+        volumeCurrency: 30000,
+        weightedPrice: 2,
+      },
+      {
+        timestamp: 100000,
+        open: NaN,
+        close: NaN,
+        high: 1000,
+        low: 99,
+        volumeBtc: 1000000,
+        volumeCurrency: 30000,
+        weightedPrice: 2,
+      },
+      {
+        timestamp: 100000,
+        open: NaN,
+        close: NaN,
+        high: 1000,
+        low: 99,
+        volumeBtc: 1000000,
+        volumeCurrency: 30000,
+        weightedPrice: 2,
+      },
+      {
+        timestamp: 100000,
+        open: NaN,
+        close: NaN,
+        high: 1000,
+        low: 99,
+        volumeBtc: 1000000,
+        volumeCurrency: 30000,
+        weightedPrice: 2,
+      },
+      {
+        timestamp: 100000,
+        open: NaN,
+        close: NaN,
+        high: 1000,
+        low: 99,
+        volumeBtc: 1000000,
+        volumeCurrency: 30000,
+        weightedPrice: 2,
+      },
+    ],
+  }),
+};
+</script>
