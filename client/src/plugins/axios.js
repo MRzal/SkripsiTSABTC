eff48429320c7f039c06f1f1827393a12c05fa3e
bicky33 <biki.hamzah@gmail.com>
2023-03-22 23:47:16 +0700

    pagination

--- client/src/plugins/axios.js
+++ client/src/plugins/axios.js
@@ -2,4 +2,5 @@ import Vue from 'vue';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
 Vue.use(VueAxios, axios);
---
+++ client/src/plugins/sweet_alert.js
@@ -0,0 +1,5 @@
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+import Vue from 'vue';
+
+Vue.use(VueSweetalert2);
